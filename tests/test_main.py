745d41b97282722045389bbb29b7601fc3596c4f
Caio Geraldes <caioaguida@protonmail.com>
2022-02-15 17:49:23 -0300

    update ignore

--- tests/test_main.py
+++
@@ -1,6 +0,0 @@
-from doc_data import main
-
-
-def test_paths():
-    assert(main.PROC_DATA_PATH == "/home/silenus/docs/Academia/Doutorado/data/doc_data/data/")
-    assert(main.DIORISIS_PATH == "/home/silenus/docs/Textos/DBs/diorisis-resources/json/phd/")
