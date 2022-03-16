ca2c6706a1c00bdc956ec444c6cfee4b60b35f97
Caio Geraldes <caioaguida@protonmail.com>
2022-03-16 13:04:03 -0300

    testable local data

--- src/conftest.py
+++ src/conftest.py
@@ -1,10 +1,1 @@
-import os
-import pytest
-import stanza
-
-
-@pytest.fixture(scope="function")
-def stanza_resources(tmpdir):
-    STANZA_RESOURCES = os.path.join(tmpdir, "stanza_resources")
-    stanza.download(lang='grc', model_dir=STANZA_RESOURCES)
-    return STANZA_RESOURCES
+pytest_plugins = "pytester"
--- tests/test_processor.py
+++ tests/test_processor.py
@@ -5,27 +5,27 @@ from stanza import Document
 from stanza.models.common.doc import Sentence, Word
 from doc_data.processor import gen_data, read_data
 
-PROC_DATA_PATH = "/home/silenus/docs/Academia/Doutorado/data/doc_data/tests/data/proc/"
 TEST_DOC = "Lysias (0540) - For Callias (005).json"
-DIOFILE = os.path.join("/home/silenus/docs/Academia/Doutorado/data/doc_data/tests/data/preproc/", TEST_DOC)  # type: ignore
-OUTNAME = os.path.join(PROC_DATA_PATH, TEST_DOC.replace("json", "pickle"))
 
+FIXTURE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "data")
 
 @pytest.fixture
-def stanza_dir(tmpdir):#, monkeypatch):
+def stanza_dir(tmpdir):
     stanza_dir = tmpdir.mkdir("stanza_resources")
     return str(stanza_dir)
 
-
-def test_gen_data(stanza_dir):
+@pytest.mark.datafiles(os.path.join(FIXTURE_DIR, TEST_DOC))
+def test_data_tools(stanza_dir, datafiles):
     stanza.download(lang='grc', package="perseus", model_dir=stanza_dir)
     nlp = stanza.Pipeline(lang="grc", package="perseus", verbose=False, depparse_batch_size=400,dir=stanza_dir)
-    gen_data(nlp, DIOFILE, OUTNAME)
-    assert(TEST_DOC.replace("json", "pickle") in os.listdir(PROC_DATA_PATH))
+    diofile = str(os.path.join(datafiles, TEST_DOC))
+    outfile = str(diofile).replace("json", "pickle")
+    gen_data(nlp, diofile, outfile)
+    assert(TEST_DOC.replace("json", "pickle") in os.listdir(FIXTURE_DIR))
 
-
-def test_read_data():
-     doc = read_data(OUTNAME)
-     assert(isinstance(doc, Document))
-     assert(isinstance(doc.sentences[0], Sentence))
-     assert(isinstance(doc.sentences[0].words[0], Word))
+    doc = read_data(outfile)
+    assert(isinstance(doc, Document))
+    assert(isinstance(doc.sentences[0], Sentence))
+    assert(isinstance(doc.sentences[0].words[0], Word))
