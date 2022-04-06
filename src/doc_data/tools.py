145a09d8893f40867b982e12f3fd48662a9f1a9b
Caio Geraldes <caioaguida@protonmail.com>
2022-04-06 06:47:45 -0300

    add tool for sentence generation from tokens

--- src/doc_data/tools.py
+++ src/doc_data/tools.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     from doc_data.db import mongo
     from doc_data.main import MONGO
 
-    db = mongo(MONGO)
-    sent_collection = db.mviquery
+    db = mongo(MONGO, "pytest")
+    sent_collection = db.test
 
-    print(gen_sent(sent_collection, "819b50475a315e2e82057c91c1f9a6e4"))
+    print(gen_sent(sent_collection, "43136bd5b0bba2b1a7d9b7301f166bca"))
