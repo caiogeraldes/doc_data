e4d132b10d6b5aaf2c2a165686b1e33908ac5dcb
Caio Geraldes <caio.geraldes@usp.br>
2022-07-27 15:36:21 -0300

    new helper tools

--- src/doc_data/main.py
+++ src/doc_data/main.py
@@ -2,16 +2,18 @@
 Entry point to generate my PhD data
 """
 import os
+from typing import Union
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DIORISIS_PATH = os.getenv("DIORISIS_PATH")
-PROC_DATA_PATH = os.getenv("PROC_DATA_PATH")
-MONGO = os.getenv("MONGO")
-STANZA_RESOURCES_DIR = os.getenv("STANZA_RESOURCES_DIR")
-MVI = os.getenv("MVI")
-LOG = os.getenv("LOG")
+DIORISIS_PATH: Union[str, None] = os.getenv("DIORISIS_PATH")
+PROC_DATA_PATH: Union[str, None] = os.getenv("PROC_DATA_PATH")
+MONGO: Union[str, None] = os.getenv("MONGO")
+STANZA_RESOURCES_DIR: Union[str, None] = os.getenv("STANZA_RESOURCES_DIR")
+MVI: Union[str, None] = os.getenv("MVI")
+LOG: Union[str, None] = os.getenv("LOG")
+MODEL: Union[str, None] = os.getenv("MODEL")
 assert DIORISIS_PATH is not None, "Path to DIORISIS unspecified"
 assert PROC_DATA_PATH is not None, "Path to serialized stanza.Documents unspecified"
 assert MONGO is not None, "MongoDB connection unspecified"
@@ -42,21 +44,24 @@ if __name__ == "__main__":  # pragma: no cover
 
     start = time.time()
 
+    if MODEL is None:
+        MODEL = "perseus"
+
     if not os.path.exists(PROC_DATA_PATH):
         logging.warning("Creating %s", {PROC_DATA_PATH})
         os.mkdir(PROC_DATA_PATH)
 
     if not os.path.exists(STANZA_RESOURCES_DIR):
         logging.info("Downloading stanza resources to: %s", STANZA_RESOURCES_DIR)
-        stanza.download(lang="grc", package="perseus", model_dir=STANZA_RESOURCES_DIR)
+        stanza.download(lang="grc", package=MODEL, model_dir=STANZA_RESOURCES_DIR)
     elif len(os.listdir(STANZA_RESOURCES_DIR)) == 0:
         logging.info("Downloading stanza resources to: %s", STANZA_RESOURCES_DIR)
-        stanza.download(lang="grc", package="perseus", model_dir=STANZA_RESOURCES_DIR)
+        stanza.download(lang="grc", package=MODEL, model_dir=STANZA_RESOURCES_DIR)
 
     logging.info("Loading NLP Pipeline")
     nlp = stanza.Pipeline(
         lang="grc",
-        package="perseus",
+        package=MODEL,
         verbose=False,
         depparse_batch_size=400,
         dir=STANZA_RESOURCES_DIR,
@@ -93,7 +98,12 @@ if __name__ == "__main__":  # pragma: no cover
         "Building MongoDB tokens collection from the data in: %s", PROC_DATA_PATH
     )
     db: Database = mongo(MONGO)
-    col: Collection = db.tokens
+
+    if MODEL == "proiel":
+        col: Collection = db.tokens_proiel
+    else:
+        col: Collection = db.tokens
+
     if col.estimated_document_count() == 0:
         write_pickle_to_mongo(PROC_DATA_PATH, col)
     else:
